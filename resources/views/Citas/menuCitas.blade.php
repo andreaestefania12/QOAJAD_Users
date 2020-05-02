d1d46d77c2bc3bce61d607ef0f4162f59f3cd5ef
Andrea Estefania <andreaestefania12@javerianacali.edu.co>
2020-05-02 16:02:33 -0500

    Menu citas

--- resources/views/Citas/menuCitas.blade.php
+++ resources/views/Citas/menuCitas.blade.php
@@ -1,17 +1,18 @@
 @extends('main')
 
 @section('content')
-    <div id="verCitas">
-    	VER CITAS
-    </div>
+	<div id="menuCitas">
+		<div id="verCitas">
+    		VER CITAS
+	    </div>
 
-    <div id="crearCitas">
-    	CREAR CITA
-    </div>
+	    <div id="crearCitas">
+	    	CREAR CITA
+	    </div>
 
-    <div id="borrarCitas">
-    	BORRAR O MODIFICAR CITAS
-    </div>
-
-    <img src="">
+	    <div id="borrarCitas">
+	    	<p>BORRAR CITAS</p>
+	    </div>
+	</div>
+    
 @endsection
