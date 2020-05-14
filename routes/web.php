bdf898613e0e30b7a0844a2792b1a6eb91d57f90
Dicson <fetex98@gmail.com>
2020-05-13 20:50:52 -0500

    Prove routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Citas.menuCitas');
 });
+
+Route::get('/name/{name}/lastname/{lastname?}', function($name, $lastname = NULL){
+    return 'HELLO ' .$name . $lastname;
+});
+
+
+Route::get('http://18.221.11.31:9090/authentication/authenticate/{username}/{password}', 
+function($username = 'juan.2114@hotmail.com', $password = 'password'){
+    return $username. $password;
+});
