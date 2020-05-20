c542f0f955dd9beb9c326a965551493e7debb706
Dicson <fetex98@gmail.com>
2020-05-20 05:29:50 -0500

    install boostrap

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class PDFController extends Controller
 {
-    //
+    // Download, Stream ( visualiza archivo en otra pestaña)
     public function PDF(){
         $pdf = \PDF::loadview('pdf');
         return $pdf->download('prueba.pdf');
